5e2a1c2add5cc2a2d7affb9b6ae33e3970e68b1e
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2021-08-10 10:20:27 -0300

    1

--- src/components/ConfigurationPage/ConfigurationPage.js
+++ src/components/ConfigurationPage/ConfigurationPage.js
@@ -2,52 +2,54 @@ import './ConfigurationPage.css';
 import Header from './../Header/Header';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { useTranslation } from "react-i18next";
 const ConfigurationPage = () => {
     const [configurations, setConfigurations] = useState([]);
+    const[t] = useTranslation("global");
     const getConfigurations = new Promise((resolve, reject) => {
         resolve([ 
-            {id: "1", name: "Platform Configuration", children:[
-                    {id: "2", name: "Account Information"},
-                    {id: "3", name: "Data Sources"},
-                    {id: "4", name: "Tags"},
-                    {id: "5", name: "Profiles"},
-                    {id: "6", name: "Users"},
-                    {id: "7", name: "Network Overview"},
-                    {id: "8", name: "Agent Command"},
-                    {id: "9", name: "Mobile Devices"},
-                    {id: "10", name: "Audit"},
-                    {id: "11", name: "Backup/Restore"},
-                    {id: "12", name: "Raw Data Export"},
-                    {id: "13", name: "System Network Configurations"},
-                    {id: "14", name: "Reset to Factory Defaults"}
+            {id: "1", previous:"ConfigurationPage.sectionOne.", name: "title", children:[
+                    {id: "2", name: "accountInformation"},
+                    {id: "3", name: "datasources"},
+                    {id: "4", name: "tags"},
+                    {id: "5", name: "securityprofiles"},
+                    {id: "6", name: "users"},
+                    {id: "7", name: "networkoverview"},
+                    {id: "8", name: "sendagentcommands"},
+                    {id: "9", name: "mobile-devices"},
+                    {id: "10", name: "audit"},
+                    {id: "11", name: "backupConfiguration"},
+                    {id: "12", name: "tagsexportconfiguration"},
+                    {id: "13", name: "systemnetworkconfiguration"},
+                    {id: "14", name: "resettofactorydefaults"}
                 ] },
-            {id: "15", name: "Information Core", children:[
-                    {id: "16", name: "Events"},
-                    {id: "17", name: "Event Categories"},
-                    {id: "18", name: "Event Codes"},
-                    {id: "19", name: "Products"},
-                    {id: "20", name: "Equipment Models"},
-                    {id: "21", name: "Equipments"},
-                    {id: "22", name: "Equipments Events"},
-                    {id: "23", name: "Equipments Products"}
+            {id: "15", previous:"ConfigurationPage.sectionTwo.", name: "title", children:[
+                    {id: "16", name: "events"},
+                    {id: "17", name: "event-categories"},
+                    {id: "18", name: "event-codes"},
+                    {id: "19", name: "schedule.products"},
+                    {id: "20", name: "equipmentmodels"},
+                    {id: "21", name: "equipments"},
+                    {id: "22", name: "equipments-events"},
+                    {id: "23", name: "schedule.equipment-products"}
                 ] },
-            {id: "24", name: "OEE Configuration", children:[
-                    {id: "25", name: "OEE Configuration"},
-                    {id: "26", name: "Delay Categories"},
-                    {id: "27", name: "Delay Codes"},
-                    {id: "28", name: "Events Codes"},
-                    {id: "29", name: "Products"},
-                    {id: "30", name: "Line Products"},
-                    {id: "31", name: "Shifts"},
-                    {id: "32", name: "Reset to Factory Defaults"},
-                    {id: "33", name: "User Manual"}
+            {id: "24", previous:"ConfigurationPage.sectionThree.", name: "title", children:[
+                    {id: "25", name: "oeeconfiguration"},
+                    {id: "26", name: "delaycategories"},
+                    {id: "27", name: "delaycodes"},
+                    {id: "28", name: "oee-event-codes"},
+                    {id: "29", name: "products"},
+                    {id: "30", name: "lineproducts"},
+                    {id: "31", name: "shifts"},
+                    {id: "32", name: "resettofactorydefaultsoee"},
+                    {id: "33", name: "usermanualoee"}
                 ] },
-            {id: "34", name: "Tools", children:[
-                    {id: "35", name: "Mimic Builder"}
+            {id: "34", previous:"ConfigurationPage.sectionFour.", name: "title", children:[
+                    {id: "35", name: "screenbuilder"}
                 ] },
-            {id: "36", name: "About", children:[
-                    {id: "37", name: "About"},
-                    {id: "38", name: "How To"}
+            {id: "36", previous:"ConfigurationPage.sectionFive.", name: "title", children:[
+                    {id: "37", name: "about"},
+                    {id: "38", name: "howto"}
                 ] }
             ])
     }); 
@@ -64,14 +66,18 @@ const ConfigurationPage = () => {
                 <div className="ConfigurationLeft">
                     {configurations.map(configuration => {
                         return(
-                            <div key={configuration.id} className="ConfigurationLeftContent" style={configuration.name=="About"? {marginBottom:"60px"} : null}>
-                                <h4>{configuration.name}</h4>
+                            <div key={configuration.id} className="ConfigurationLeftContent" style={configuration.previous=="ConfigurationPage.sectionFive."? {marginBottom:"60px"} : null}>
+                                <h4>{t(configuration.previous+configuration.name)}</h4>
                                 <ul className="Options">
                                     {configuration.children.map(option => {
                                         return(
-                                            <li key={option.id}>
-                                                <Link to={`/configuration/${option.name}`}>{option.name}</Link>
-                                            </li>
+                                            <>
+                                                <Link to={`/configuration/${option.name}`}>
+                                                    <li key={option.id}>
+                                                        {t(configuration.previous+option.name)}
+                                                    </li>    
+                                                </Link>
+                                            </>
                                         )
                                     })}
                                 </ul>
